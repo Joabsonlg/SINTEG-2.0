06ca9dbb4954aec0a6564a61b238893d0a24c44f
Joabsonlg <deyvissonaislan@outlook.com>
2018-10-11 15:44:40 -0300

    Barra de ações.

--- app/controllers/Professores.java
+++ app/controllers/Professores.java
@@ -26,14 +26,14 @@ public class Professores extends Controller{
 		render();
 	}
 	
-	@Administrador
+	//@Administrador
 	public static void formCadProfessor() {
 		List<Uf> ufs = Uf.findAll();
 		List<Escolaridade> niveis = Escolaridade.findAll();
 		render(ufs, niveis);
 	}
 	
-	@Administrador
+	//@Administrador
 	public static void cadProfessor(Professor professor) {
 		professor.save();
 		detailProfessor(professor.id);
@@ -44,14 +44,14 @@ public class Professores extends Controller{
 		render(professores);
 	}
 	
-	@Administrador
+	//@Administrador
 	public static void removeProfessor(Long id) {
 		Professor prof = Professor.findById(id);
 		prof.delete();
 		listProfessor();
 	}
 	
-	@Administrador
+	//@Administrador
 	public static void editProfessor(Long id) {
 
 	}
--- app/controllers/Settings.java
+++ app/controllers/Settings.java
@@ -1,5 +1,14 @@
 package controllers;
 
-public class Settings {
+import play.mvc.Controller;
+import play.mvc.With;
 
+@With(Secure.class)
+@Administrador
+public class Settings extends Controller{
+	
+	public static void index() {
+		render();
+	}
+	
 }
